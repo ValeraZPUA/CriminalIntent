517f4f7b89430b55a6414489a3cdc06e85b2e7d6
Valera Lyuty <lvv0490@gmail.com>
2019-03-22 11:39:50 +0200

    add abstract class for creating new activities; change CrimeActivity;

--- app/src/main/java/edu/example/criminalintent/CrimeListActivity.java
+++ app/src/main/java/edu/example/criminalintent/CrimeListActivity.java
@@ -1,15 +1,13 @@
 package edu.example.criminalintent;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CrimeActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
---
+++ app/src/main/java/edu/example/criminalintent/CrimeListFragment.java
@@ -0,0 +1,7 @@
+package edu.example.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+class CrimeListFragment extends Fragment {
+
+}
